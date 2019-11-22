434fd165588e17bd197c73ac0c47d2789b212c0d
Alexander Solovey <alexandersolovey@gmail.com>
2019-11-22 13:09:57 -0500

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into todo(id, username,description,target_date,is_done)
+values(10001, 'in28minutes', 'Learn JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10002, 'in28minutes', 'Learn Data JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10003, 'in28minutes', 'Learn Microservices', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10004, 'alex', 'Learn AWS', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10005, 'dian', 'Learn Java', sysdate(), false);
